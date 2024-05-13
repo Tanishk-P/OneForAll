b737fec049361aa9b0722da5dc745624cd941169
Tanishk-P <tanishkparadkar@gmail.com>
2024-05-13 12:58:35 +0530

    new changes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import { textStyles } from './src/Utils/StyleSheets/textStyles';
+import {SafeAreaView, Text, View} from 'react-native';
+import {textStyles} from './src/Utils/StyleSheets/textStyles';
 
 export default function App(): JSX.Element {
   return (
     <View>
       <SafeAreaView />
+      <Text>Hi</Text>
       <Text style={textStyles.text}>Hello</Text>
     </View>
   );
